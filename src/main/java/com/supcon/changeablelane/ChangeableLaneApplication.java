2efaa4d6fd0ab293b4fb24363594f44482d6549c
JiangWangfa <jiangwangfa@supcon.com>
2021-02-23 13:49:27 +0800

    first commit

---
+++ src/main/java/com/supcon/changeablelane/ChangeableLaneApplication.java
@@ -0,0 +1,13 @@
+package com.supcon.changeablelane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeableLaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChangeableLaneApplication.class, args);
+    }
+
+}
